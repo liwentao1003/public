da2436ec3ca3502bdc7d8697e59f7cb10f88518c
liwentao <liwentao@300.cn>
2020-12-28 14:38:28 +0800

    feat: test

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,23 @@
-module.exports = {extends: ['@commitlint/config-conventional']};
+//module.exports = {extends: ['@commitlint/config-conventional']};
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'subject-case': [0, 'never'],
+        'type-enum': [
+            2,                  //代表必须输入
+            'always',
+            [
+                "docs",     // 仅仅修改了文档，比如README, CHANGELOG, CONTRIBUTE等等
+                "chore",    // 改变构建流程、或者增加依赖库、工具等
+                "feat",     // 新增feature
+                "fix",      // 修复bug
+                "merge",    // Merge branch ? of ?.
+                "perf",     // 优化相关，比如提升性能、体验
+                "refactor", //  代码重构，没有加新功能或者修复bug
+                "revert",    //  回滚到上一个版本
+                "style",    // 仅仅修改了空格、格式缩进、都好等等，不改变代码逻辑
+                "test"     //  测试用例，包括单元测试、集成测试等
+            ]
+        ]
+    }
+};
