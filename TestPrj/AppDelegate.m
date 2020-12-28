41ef4119a16af28f1201007e32bf0fa491a3d8bf
liwentao <liwentao@300.cn>
2020-12-28 14:59:17 +0800

    test

--- TestPrj/AppDelegate.m
+++ TestPrj/AppDelegate.m
@@ -17,7 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    //testtest
+    //testtestee
     return YES;
 }
 
